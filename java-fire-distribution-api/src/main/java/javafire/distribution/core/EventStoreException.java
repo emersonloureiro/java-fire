a305ee66faa1c0c2b61225c46352c1ee24cdf30d
emerson.loureiro@gmail.com <emerson.loureiro@gmail.com@df64c986-4bcf-4a0c-3c66-bf0a89a323df>
2013-04-27 19:36:05 +0000

    Putting everything under a single project!
    git-svn-id: http://java-fire.googlecode.com/svn/trunk/java-fire@68 df64c986-4bcf-4a0c-3c66-bf0a89a323df

--- java-fire-distribution-api/src/main/java/javafire/distribution/core/EventStoreException.java
+++
@@ -1,9 +0,0 @@
-package javafire.distribution.core;
-
-/**
- * Class of exception thrown by operations involving the event store.
- * 
- * @author Emerson Loureiro
- */
-public class EventStoreException extends Exception {
-}
