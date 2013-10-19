dff294d940f893410ed6de4bb05602a4c619690e
emerson.loureiro@gmail.com <emerson.loureiro@gmail.com@df64c986-4bcf-4a0c-3c66-bf0a89a323df>
2013-10-19 11:09:59 +0000

    Updating javadocs that referenced asynchronous events.
    git-svn-id: http://java-fire.googlecode.com/svn/trunk/java-fire@80 df64c986-4bcf-4a0c-3c66-bf0a89a323df

--- java-fire-core/src/main/java/javafire/eventing/core/EventTransaction.java
+++ java-fire-core/src/main/java/javafire/eventing/core/EventTransaction.java
@@ -10,19 +10,16 @@ public interface EventTransaction {
 	/**
 	 * Commits this transaction, processing all events fired within it.
 	 * Synchronous events will be processed sequentially on the order they've
-	 * been fired (i.e., FIFO), whereas asynchronous ones will be serialized and
-	 * dispatched to be processed on separate transactions, possibly remotely.
-	 * Any {@link Exception} thrown while processing any of the synchronous
-	 * events, or dispatching any of the asynchronous ones, will automatically
-	 * cause this transaction to rollback, and a {@link TransactionException} to
-	 * be thrown. See {@link EventTransaction#rollback()}.
+	 * been fired (i.e., FIFO). Any {@link Exception} thrown while processing
+	 * any of the synchronous events will automatically cause this transaction
+	 * to rollback, and a {@link TransactionException} to be thrown. See
+	 * {@link EventTransaction#rollback()}.
 	 * 
-	 * @throws EventException
-	 *             If there's any error processing/dispatching the events for
-	 *             this transaction, including errors within the handlers for
-	 *             each event.
-	 * @throws TransactionException
-	 *             If there's any other error when committing the transaction
+	 * @throws EventException If there's any error processing/dispatching the
+	 *             events for this transaction, including errors within the
+	 *             handlers for each event.
+	 * @throws TransactionException If there's any other error when committing
+	 *             the transaction
 	 */
 	void commit() throws EventException, TransactionException;
 
